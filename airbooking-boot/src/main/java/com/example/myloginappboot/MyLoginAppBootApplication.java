352cfe50718caed974a2ec5e809ea2383aaa28b5
YOUNYOUNGBU <dudqn9258@g.shingu.ac.kr>
2025-05-11 01:19:41 +0900

    airbooking-boot ver2

---
+++ airbooking-boot/src/main/java/com/example/myloginappboot/MyLoginAppBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.myloginappboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLoginAppBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLoginAppBootApplication.class, args);
+    }
+
+}
---
+++ airbooking-boot/src/test/java/com/example/myloginappboot/MyLoginAppBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myloginappboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLoginAppBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
